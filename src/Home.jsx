dd86dcf5dc9eb127cfbfe39a80e5dcc4f9c6a9e8
Johnsamuel24 <vmprasanth1999@gmail.com>
2023-08-08 17:42:28 +0530

    firstcommit

---
+++ src/Home.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        React.createElement('div',{className:'container-fluid'},React.createElement('div',{className:'p1'},'Airtel Fiber Xtream'),React.createElement('div',{className:'card-group'},React.createElement('div',{className:'card'}
+        
+        ,React.createElement('div',{className:"card-body"}
+       
+        ,React.createElement('h1',null,'Plan 1')
+        ,React.createElement('h2',{className:"card-title"},'₹ 999 '
+        ,React.createElement('span',{className:"period"},'/ month'))
+        ,React.createElement('hr',null)
+        ,React.createElement('p',{className:"card-text"}
+        
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Internet Upto 200Mbps')
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Local/STD Calls')
+        
+        ,React.createElement('div',{id:'d2'},'Includes Airtel Thanks benefit:')
+     
+        ,React.createElement('div',{id:'d23'},'Amazon Prime Video')
+        ,React.createElement('div',{id:'d23'},'Disney+ Hotstar')
+   
+     
+        ,React.createElement('div',{id:'d24',className:'card-footer text-body-secondary'}
+        ,React.createElement('Button',{className:"mb-2", type:"Button"},'Buy Plan')
+        ,React.createElement('div',{className:'link'}
+        ,React.createElement('a',{ href:'https://www.airtel.in/broadband/'},'Know More')
+        )
+        )
+        )
+      
+        )
+        )
+        
+   
+        
+        ,React.createElement('div',{className:'card'}
+        
+        ,React.createElement('div',{className:"card-body"}
+       
+        ,React.createElement('h1',null,'Plan 1')
+        ,React.createElement('h2',{className:"card-title"},'₹ 1999 '
+        ,React.createElement('span',{className:"period"},'/ month'))
+        ,React.createElement('hr',null)
+        ,React.createElement('p',{className:"card-text"}
+        
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Internet Upto 500Mbps + Additional Benefits')
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Local/STD Calls')
+        
+        ,React.createElement('div',{id:'d2'},'Includes Airtel Thanks benefit:')
+     
+        ,React.createElement('div',{id:'d23'},'Amazon Prime Video')
+        ,React.createElement('div',{id:'d23'},'Disney+ Hotstar')
+    
+        ,React.createElement('div',{id:'d23'},'Wynk Premium')
+        ,React.createElement('div',{id:'d25',className:'card-footer text-body-secondary'}
+        ,React.createElement('Button',{className:"mb-2", type:"Button"},'Buy Plan')
+        ,React.createElement('div',{className:'link'}
+        ,React.createElement('a',{ href:'https://www.airtel.in/broadband/'},'Know More')
+        )
+      
+        
+        )
+        )
+      
+        )
+      
+        )
+        
+        
+        ,React.createElement('div',{className:'card'}
+        
+        ,React.createElement('div',{className:"card-body"}
+       
+        ,React.createElement('h1',null,'Plan 1')
+        ,React.createElement('h2',{className:"card-title"},'₹ 2999 '
+        ,React.createElement('span',{className:"period"},'/ month'))
+        ,React.createElement('hr',null)
+        ,React.createElement('p',{className:"card-text"}
+        
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Internet Upto 100Mbps')
+        
+        ,React.createElement('div',{id:'d1'},'Unlimited Local/STD Calls')
+        
+        ,React.createElement('div',{id:'d2'},'Includes Airtel Thanks benefit:')
+     
+        ,React.createElement('div',{id:'d23'},'Amazon Prime Video')
+        ,React.createElement('div',{id:'d23'},'Disney+ Hotstar')
+        ,React.createElement('div',{id:'d23'},'Xstream Premium Pack')
+        ,React.createElement('div',{id:'d23'},'Wynk Premium')
+        ,React.createElement('div',{id:'d23',className:'card-footer text-body-secondary'}
+        ,React.createElement('Button',{className:"mb-2", type:"Button"},'Buy Plan'))
+        ,React.createElement('div',{className:'link'}
+        ,React.createElement('a',{href:'https://www.airtel.in/broadband/'},'Know More')
+        )
+      
+        
+        
+        )
+      
+        )
+      
+        ))
+        
+        ));
+    
+}
+
+export default Home
